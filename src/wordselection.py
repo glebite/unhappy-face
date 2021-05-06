c93a94551f6dac6d716c1314e32cabdce684ec5d
glebite <glebite@gmail.com>
2021-05-06 08:26:10 -0400

    Changed out method name, cleanup, some more documentation to make it easier to understand.

--- src/wordselection.py
+++ src/wordselection.py
@@ -3,6 +3,7 @@
 """
 import sys
 import csv
+import random
 
 
 class WordSelection(object):
@@ -12,6 +13,7 @@ class WordSelection(object):
         """
         """
         self.file_name = arguments
+        self.word_bag = dict()
 
     def __str__(self):
         """
@@ -23,20 +25,36 @@ class WordSelection(object):
         """
         return print(f'{self}')
 
-    def read_list(self):
-        """
+    def read_file(self):
+        """read_file - reads a .csv file filled with words
+
+        Word list files are in .csv (, comma) delimitered according to:
+        English, Farsi, English Description
+        ...
+        EOF
+
+        A dictionary is created from the column values with end whitespaces
+        stripped as part of the data cleaning:
+        Farsi: (English, English Description)
+
+        param:  None
+        return: None
         """
         with open(self.file_name, newline='') as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
-            for row in reader:
-                print(', '.join(row))
+            self.word_bag = {r[1].strip(): (r[0].strip(), r[2].strip())
+                             for r in reader}
+        print(self.word_bag)
+
+    def pick_word(self):
+        pass
 
 
 def main(arguments):
     """
     """
     x = WordSelection(arguments)
-    x.read_list()
+    x.read_file()
 
 
 if __name__ == "__main__":
