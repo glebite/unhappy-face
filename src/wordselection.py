45a653f2d551e6852d4388b6eca97b04605d901b
glebite <glebite@gmail.com>
2021-05-05 20:37:41 -0400

    module for importing word files, and organizing things

---
+++ src/wordselection.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+"""
+import sys
+import csv
+
+
+class WordSelection(object):
+    def __init__(self, arguments):
+        pass
+
+
+def main(arguments):
+    pass
+
+
+if __name__ == "__main__":
+    main(sys.argv)
