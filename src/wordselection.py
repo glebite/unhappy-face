d81478de50289a509a216f54627b3aff9df81b25
glebite <glebite@gmail.com>
2021-05-05 21:04:05 -0400

    Reading in the file for now...

--- src/wordselection.py
+++ src/wordselection.py
@@ -6,13 +6,38 @@ import csv
 
 
 class WordSelection(object):
+    """
+    """
     def __init__(self, arguments):
-        pass
+        """
+        """
+        self.file_name = arguments
+
+    def __str__(self):
+        """
+        """
+        print(f'{self.file_name=}')
+
+    def __repr__(self):
+        """
+        """
+        return print(f'{self}')
+
+    def read_list(self):
+        """
+        """
+        with open(self.file_name, newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row in reader:
+                print(', '.join(row))
 
 
 def main(arguments):
-    pass
+    """
+    """
+    x = WordSelection(arguments)
+    x.read_list()
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main('../data/nouns.txt')
