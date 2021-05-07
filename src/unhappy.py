79811f2a3d2a2a1a589c7a453f0b3ccdf85b9c09
glebite <glebite@gmail.com>
2021-05-07 11:33:15 -0400

    Creation of the base class for the displaying of the unhappy face.

---
+++ src/unhappy.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+"""
+
+
+class Unhappy(object):
+    """
+    """
+    def __init__(self, arguments=None):
+        """__init__ -
+        """
+        pass
+
+    def draw(self):
+        """draw -
+        """
+        pass
+
+
+def main():
+    """
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
