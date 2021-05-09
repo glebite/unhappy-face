3918aba14ccd58c0a661b2bc43ac3f3b2d05300e
glebite <glebite@gmail.com>
2021-05-09 04:51:46 -0400

    fleshing out this functionality

--- src/unhappy.py
+++ src/unhappy.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
+"""unhappy.py - base class for displaying unhappy face
 """
-"""
+
+
+NORMALGAME = 6
 
 
 class Unhappy(object):
     """
     """
-    def __init__(self, arguments=None):
+    def __init__(self, n_pieces=NORMALGAME):
         """__init__ -
+        """
+        self.n_pieces = n_pieces
+        self.to_draw = 0
+
+    def draw(self):
+        """draw - base unhappy face
         """
         pass
 
-    def draw(self):
-        """draw -
-        """
-        pass
+    def __str__(self):
+        return f'{n_pieces=} {to_draw=}')
 
 
 def main():
