741a845249ef24d8124697e79acd8593a8a9a20a
glebite <glebite@gmail.com>
2021-05-10 07:32:51 -0400

    Cleaning up __str__ and __repr__ methods

--- src/unhappy.py
+++ src/unhappy.py
@@ -21,7 +21,10 @@ class Unhappy(object):
         pass
 
     def __str__(self):
-        return f'{n_pieces=} {to_draw=}')
+        return f'%={self.to_draw/self.n_pieces}'
+
+    def __repr__(self):
+        return f'Unhappy(n_pieces={self.n_pieces})'
 
 
 def main():
