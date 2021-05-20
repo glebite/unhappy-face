958e79a9b81e21c9601b3c94965d3ab99dde03b7
glebite <glebite@gmail.com>
2021-05-20 15:40:25 -0400

    Added in second simple test

--- test/test_unhappy.py
+++ test/test_unhappy.py
@@ -2,10 +2,18 @@ import unittest
 import unhappy
 
 
+BIGNUMBEROFPIECES = 13
+
+
 class Testing(unittest.TestCase):
     def test_creation(self):
         x = unhappy.Unhappy()
         self.assertNotEqual(x, None)
+
+    def test_creation_pieces_set(self):
+        x = unhappy.Unhappy(BIGNUMBEROFPIECES)
+        self.assertEqual(x.n_pieces, BIGNUMBEROFPIECES)
+
 
 if __name__ == '__main__':
     unittest.main()
