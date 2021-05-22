817ce2dbc232fac4625e6f7d0dc44beca91b4785
glebite <glebite@gmail.com>
2021-05-21 22:17:29 -0400

    More tests before the storm...

--- test/test_wordselection.py
+++ test/test_wordselection.py
@@ -7,6 +7,19 @@ class Testing(unittest.TestCase):
         x = wordselection.WordSelection('nonce')
         self.assertNotEqual(x, None)
 
+    def test_confirm_arguments(self):
+        x = wordselection.WordSelection('nonce')
+        self.assertEqual(x.file_name, 'nonce')
+
+    def test_confirm_farsi_empty(self):
+        x = wordselection.WordSelection('nonce')
+        self.assertEqual(x.farsi, '')
+
+    def test_confirm_check_guess(self):
+        x = wordselection.WordSelection('nonce')
+        x.farsi = ' شهر'
+        self.assertEqual(x.check_guess('ه'), True)
+
 
 if __name__ == '__main__':
     unittest.main()
